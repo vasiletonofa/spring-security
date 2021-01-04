f07e2a60340ce84c555c700fee4cb8fc5c46d91a
vasiletonofa <vasile.tonofa@gmail.com>
2021-01-04 13:31:42 +0200

    init project

---
+++ ss-c1/src/main/java/com/example/ssc1/config/ProjectConfig.java
@@ -0,0 +1,7 @@
+package com.example.ssc1.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProjectConfig {
+}
---
+++ ss-c1/src/test/java/com/example/ssc1/SsC1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ssc1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsC1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
