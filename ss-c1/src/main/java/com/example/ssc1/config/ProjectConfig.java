c4dfccff04487449ba854bba0ba2b3b61880e09a
vasile <vasile.tonofa@gmail.com>
2021-01-05 17:04:23 +0200

    UserDetailsService

--- ss-c1/src/main/java/com/example/ssc1/config/ProjectConfig.java
+++ ss-c1/src/main/java/com/example/ssc1/config/ProjectConfig.java
@@ -1,7 +1,22 @@
 package com.example.ssc1.config;
 
+import com.example.ssc1.services.JPAUserDetailsService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class ProjectConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new JPAUserDetailsService();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
