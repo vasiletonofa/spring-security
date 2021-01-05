5d425234be21245fb75706a5b24b09ee27b5295d
vasile <vasile.tonofa@gmail.com>
2021-01-05 17:58:31 +0200

    JdbcUserDetailsManager

--- ss-c1/src/main/java/com/example/ssc1/config/ProjectConfig.java
+++ ss-c1/src/main/java/com/example/ssc1/config/ProjectConfig.java
@@ -1,18 +1,24 @@
 package com.example.ssc1.config;
 
-import com.example.ssc1.services.JPAUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class ProjectConfig {
 
+    @Autowired
+    private DataSource dataSource;
+
     @Bean
     public UserDetailsService userDetailsService() {
-        return new JPAUserDetailsService();
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
